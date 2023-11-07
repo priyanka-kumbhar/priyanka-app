3ea77a326e23542efa2614c9b873124a6182b5fe
priyanka-kumbhar <149798155+priyanka-kumbhar@users.noreply.github.com>
2023-11-07 17:54:03 +0530

    Update _app.py

--- _app.py
+++ _app.py
@@ -1,6 +1,7 @@
+import pandas as pd
 import streamlit as st
-from fbprophet import prophet
+from prophet import prophet
 # import numpy as py
-import pandas as pd 
+ 
 data = pd.write_csv('/content/Leave_tracker1.py.csv)   
 data.head()
