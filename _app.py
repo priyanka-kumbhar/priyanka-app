c1308f24b96cbb184cefef2b45d6d242a3f23504
priyanka-kumbhar <149798155+priyanka-kumbhar@users.noreply.github.com>
2023-11-07 18:13:45 +0530

    Update _app.py

--- _app.py
+++ _app.py
@@ -1,10 +1,11 @@
+import numpy as np
 import pandas as pd
 import streamlit as st
 # from prophet import Prophet
-import numpy as np
 
-import matplotlib.pyplot as plt
-%matplotlib inline
+
+# import matplotlib.pyplot as plt
+# %matplotlib inline
 
 df = pd.write_csv('Leave_tracker1.py.csv')   
 df.head() 
