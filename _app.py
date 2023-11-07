83331c2f19a77bec0f0bf8e73a9af89bbd34c4bd
priyanka-kumbhar <149798155+priyanka-kumbhar@users.noreply.github.com>
2023-11-07 18:54:42 +0530

    Update _app.py

--- _app.py
+++ _app.py
@@ -1,11 +1,12 @@
-import numpy as np
-import pandas as pd
+# import numpy as np
 import streamlit as st
+import pandas as pd
+
 # from prophet import Prophet
 
 
 # import matplotlib.pyplot as plt
 # %matplotlib inline
 
-df=pd.read_csv('Leave_tracker1.py.csv')   
-df.head() 
+df=pd.read_csv("Leave_tracker1.py.csv")   
+# df.head() 
