cfac3461e1594f0a40ffbd663471af1e4ebc48b6
priyanka-kumbhar <149798155+priyanka-kumbhar@users.noreply.github.com>
2023-11-08 15:10:49 +0530

    Update _app.py

--- _app.py
+++ _app.py
@@ -1,19 +1,20 @@
 # import numpy as np
 import streamlit as st
 import pandas as pd
+from prophet import Prophet
 
-# from prophet import Prophet
-
+data = pd.read_csv("Leave_tracker1.py.csv")
+st.write(data)
 
 # import matplotlib.pyplot as plt
 # %matplotlib inline
 
-df=pd.read_csv("Leave_tracker1.py.csv")  
-st.write(df)
+# df=pd.read_csv("Leave_tracker1.py.csv")  
+# st.write(df)
 # df.head() 
 # df.info()
 # st.write(df)
 
 # import matplotlib.pyplot as plt
 # plt.figure(figsize=(15,6))
-# plt.plot()
+# plt.plot(df=)
